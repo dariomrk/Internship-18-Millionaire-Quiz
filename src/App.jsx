1525b1148cb80a87e1f93254918c567561c43584
dariomrk <dariomrkonjic1@gmail.com>
2023-04-03 20:25:16 +0200

    Scaffold App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import ScoreProvider from './providers/ScoreProvider';
+import JokerProvider from './providers/JokerProvider';
+import QuestionProvider from './providers/QuestionProvider';
 
 function App() {
   return (
-    <div />
+    <ScoreProvider>
+      <JokerProvider>
+        <QuestionProvider>
+          {/* application */}
+        </QuestionProvider>
+      </JokerProvider>
+    </ScoreProvider>
   );
 }
 
