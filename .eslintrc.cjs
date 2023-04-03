53796ee254ede1054019320db368e289283f5dee
dariomrk <dariomrkonjic1@gmail.com>
2023-04-03 14:22:51 +0200

    Add and setup eslint.

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'airbnb',
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/prop-types': ['off', {}],
+    'react/jsx-props-no-spreading': ['off'],
+  },
+};
