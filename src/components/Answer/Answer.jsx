c8e9761946cd6c4f68a26a65ed7d6ea52a10b0d7
dariomrk <dariomrkonjic1@gmail.com>
2023-04-05 17:32:32 +0200

    Fix typo in project name.

--- src/components/Answer/Answer.jsx
+++ src/components/Answer/Answer.jsx
@@ -1,9 +1,14 @@
 import { Button } from '@mantine/core';
 import React from 'react';
+import { useQuestion } from '../../providers/QuestionProvider';
+import { useScore } from '../../providers/ScoreProvider';
 
 function Answer({
   answer, index, openModalCallback, setAnswerCallback, disabled,
 }) {
+  const questionContext = useQuestion();
+  const scoreContext = useScore();
+
   return (
     <Button
       variant="light"
